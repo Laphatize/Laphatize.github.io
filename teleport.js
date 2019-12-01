d4f0ade9ab590558fe12bc8f6fb7363f40b4d934
Pranav Ramesh <pranavramesh@getjola.me>
2019-11-30 19:38:25 -0500

    e
    e

---
+++ teleport.js
@@ -0,0 +1,6 @@
+// I bet you had big expectations for this file. :)
+
+function teleport() {
+  var code = document.getElementById("teleport").value;
+  window.location.href = `./to/${code}.html`;
+}
